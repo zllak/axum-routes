59ecbf83c38dbedeee4ffda9af03fc13a704e442
Thomas Meson <zllak@hycik.org>
2025-05-02 20:25:25 +0200

    sdf

--- axum-routes/examples/customize.rs
+++ axum-routes/examples/customize.rs
@@ -22,4 +22,15 @@ enum Nested {
 
 fn main() {
     let _routes = axum_routes::router!(Router, home_customizer = |route| route);
+
+    router!(Router,
+        Router::Home => |route| route,
+        Router::Nested(_) => |router| router,
+        Router::Nested(Nested::OtherHome) => |route| route,
+        Router::Nested(Nested::OtherNested(_)) => |router| router,
+    );
+
+    // Somehow transform this into a HashMap<name, BoxedFn...>
+    // and do that for top level Router, then Router::Nested, then
+    // Router::Nested(Nested::OtherNested) ...
 }
